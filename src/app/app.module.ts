39572762a02203bb703cdd4188c4a2b444167ddc
Thasniya Beevi.T <thasniyabeevit@gmail.com>
2022-01-31 15:15:24 +0530

    Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
+import { DataService } from './services/data.service';
+import { PaginationComponent } from './pagination/pagination.component';
+import { ListitemComponent } from './listitem/listitem.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { AskquestionComponent } from './askquestion/askquestion.component';
+import { EditanswerComponent } from './editanswer/editanswer.component';
+import { EditquestionComponent } from './editquestion/editquestion.component';
+import { MyanswersComponent } from './myanswers/myanswers.component';
+import { MyquestionsComponent } from './myquestions/myquestions.component';
+import { ShowquestionComponent } from './showquestion/showquestion.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginationComponent,
+    ListitemComponent,
+    HomeComponent,
+    AuthComponent,
+    AskquestionComponent,
+    EditanswerComponent,
+    EditquestionComponent,
+    MyanswersComponent,
+    MyquestionsComponent,
+    ShowquestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService,DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
